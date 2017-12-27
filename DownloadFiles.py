72f470b2cc12f4594afe3e1c1046f10f9262d0dd
tejas1996 <tejas.dond@ves.ac.in>
2017-12-27 23:58:43 +0530

    "downloading files from internet"

--- DownloadFiles.py
+++ DownloadFiles.py
@@ -1,10 +1,21 @@
-import random
-import urllib.request
+# This is a simple file downloader in which all you have to give is the url of the file to be downloaded and its type(Eg: pdf,jpg)
+# Demo Input:
+# url = http://newsonair.nic.in/GST_FAQ.pdf
+# filetype = pdf
 
-def download_web(url):
-    name = random.randrange(1,1000);
-    filetype = "pdf"
-    fullname = str(name) + filetype
+
+import urllib.request
+from pip._vendor.distlib.compat import raw_input
+
+def download_web(url,filetype):
+    name = "downloadedFile"
+    fullname = name + "."+ filetype
     urllib.request.urlretrieve(url, fullname)
 
-download_web("http://newsonair.nic.in/GST_FAQ.pdf")
+
+url = raw_input("enter the url from where you want to download")
+type(url)
+filetype = raw_input("enter the fileType")
+type(filetype)
+
+download_web(url,filetype)
